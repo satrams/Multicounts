bbfa8df382f79cd649c8a6e75ae36a687e1c2bcb
satrams <mememaster420noscope360@gmail.com>
2022-03-14 18:09:54 -0400

    More concise
    Just uses the folder name from the directory lol

--- ConfigureRespoitory.bat
+++ ConfigureRespoitory.bat
@@ -4,12 +4,11 @@ set /p directory=Enter the directory of the repo:
 set /p name=Enter the username of the account you want to use:
 set /p email=Enter the email of the account you want to use:
 set /p host=Enter the host you set up your key to use (see the config in `USER/.ssh`):
-set /p repo=Enter the name of the repository:
 
 cd %directory%
 git config user.name %name%
 git config user.email %email%
-git remote add origin git@%host%:%name%/%repo%.git
+git remote set-url origin git@%host%:%name%/%~n1.git
 
 echo finished
 pause
