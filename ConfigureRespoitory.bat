2eca52e8060c145192e6b17f3713037875310530
satrams <mememaster420noscope360@gmail.com>
2022-03-14 18:47:15 -0400

    Fixed origin set-url issues

--- ConfigureRespoitory.bat
+++ ConfigureRespoitory.bat
@@ -1,14 +1,15 @@
 @echo off
 
-set /p directory=Enter the directory of the repo:
+set /p directory=Enter the directory of the repo (assumes remote directory is the same):
 set /p name=Enter the username of the account you want to use:
 set /p email=Enter the email of the account you want to use:
 set /p host=Enter the host you set up your key to use (see the config in `USER/.ssh`):
 
 cd %directory%
+FOR /D %%I in ("%CD%") do set file=%%~nI
 git config user.name %name%
 git config user.email %email%
-git remote set-url origin git@%host%:%name%/%~n1.git
+git remote set-url origin git@%host%:%name%/%file%.git
 
 echo finished
 pause
